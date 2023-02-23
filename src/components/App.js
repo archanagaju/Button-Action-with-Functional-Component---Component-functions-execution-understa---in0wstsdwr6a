ccd12a59bf23275451c6622c8d6fdb7fef84cba0
archanagaju <121249232+archanagaju@users.noreply.github.com>
2023-02-23 02:43:11 -0800

    done

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,27 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [renderPara, setRenderPara] = useState(false);
+
+  const buttonClickHandler = () => {
+    setRenderPara(true);
+  }
+
+  const renderParaorButton = () => {
+    if(renderPara) {
+      return <p id="para">
+        Hello, I've learnt to use the full-stack evaluation tool.
+        This makes me so happy</p>
+    } else {
+      return <button id="click" onClick={buttonClickHandler}>Click Me</button>
+    }
+  }
+
+
   return (
     <div id="main">
       // Do not alter the main div
+      {renderParaorButton()}
     </div>
   );
 }
